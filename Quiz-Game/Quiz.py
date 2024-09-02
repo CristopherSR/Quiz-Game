543bfbb0998f34991220d5c71587e59227a029c1
cristopher <cristofitor1274@hotmail.com>
2024-09-02 18:50:01 -0500

    Questions and save the answers

--- Quiz-Game/Quiz.py
+++ Quiz-Game/Quiz.py
@@ -1,17 +1,19 @@
-question1 = ""
-question2 = ""
-question3 = ""
-question4 = ""
-question5 = ""
-question6 = ""
-question7 = ""
-question8 = ""
-question9 = ""
-
+# Start of the game 
 print("Welcome to my Quiz-Game")
 play = input("Are you ready to play?")
 
-if play.lower() == "yes":
-    print("Let's start!")
-else:
+if play.lower() != "yes":
     print("Okey run the game again, whenever your ready!")
+    quit()
+
+print("Let's play!")
+
+# Questions and save the answers
+
+answer1 = input("What does PIP stand for")
+answer2 = input("What does the \"#\" symbol do in Python?")
+answer3 = input("Is indentation requiered in Python?")
+answer4 = input("What is a break in Python")
+answer5 = input("Name one of the pupular applications of Python")
+
+
