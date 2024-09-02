8cca5f0c203a7cfb8719bee32be0aa28e95f872b
cristopher <cristofitor1274@hotmail.com>
2024-09-02 18:23:58 -0500

    if you are ready to play

---
+++ Quiz-Game/Quiz.py
@@ -0,0 +1,17 @@
+question1 = ""
+question2 = ""
+question3 = ""
+question4 = ""
+question5 = ""
+question6 = ""
+question7 = ""
+question8 = ""
+question9 = ""
+
+print("Welcome to my Quiz-Game")
+play = input("Are you ready to play?")
+
+if play.lower() == "yes":
+    print("Let's start!")
+else:
+    print("Okey run the game again, whenever your ready!")
