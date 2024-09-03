74f23f24f1b92f23f621dd6d08977e203e21abaf
cristopher <cristofitor1274@hotmail.com>
2024-09-02 19:54:16 -0500

    End game

--- Quiz-Game/Quiz.py
+++ Quiz-Game/Quiz.py
@@ -10,10 +10,47 @@ print("Let's play!")
 
 # Questions and save the answers
 
-answer1 = input("What does PIP stand for")
-answer2 = input("What does the \"#\" symbol do in Python?")
-answer3 = input("Is indentation requiered in Python?")
-answer4 = input("What is a break in Python")
-answer5 = input("Name one of the pupular applications of Python")
+correct = "Correct!"
+incorrect = "Incorrect!"
+score = 0
+
+answer = input("What does PIP stand for ")
+if answer.lower() == "python installer package":
+    print(correct)
+    score += 1
+else:
+    print(incorrect)
+
+answer = input("What does the \"#\" symbol do in Python? ")
+if answer.lower() == "comment a line":
+    print(correct)
+    score += 1
+else:
+    print(incorrect)
+
+answer = input("Is indentation requiered in Python? ")
+if answer.lower() == "yes":
+    print(correct)
+    score += 1
+else:
+    print(incorrect)
+
+answer = input("To terminate the loop or a statement we use? ")
+if answer.lower() == "break":
+    print(correct)
+    score += 1
+else:
+    print(incorrect)
+    
+answer = input("Name one of the pupular applications of Python. ")
+if answer.lower() == "system scripting" or answer.lower() == "software development":
+    print(correct)
+    score += 1
+else:
+    print(incorrect)
+
+
+print("Your final score is: " +str(score)+ "/5")
+print(str((score / 5) * 100) + "%")
 
 
